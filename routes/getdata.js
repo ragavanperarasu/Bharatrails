c02e2a3c110494f5fa421eeee1f4d632935bfca9
ragavanperarasu <ragavanperarasu5761@gmail.com>
2025-10-12 22:25:35 +0530

    modified:   routes/getdata.js

--- routes/getdata.js
+++ routes/getdata.js
@@ -2,22 +2,27 @@ const productModel = require('../model/prodmodel');
 
 module.exports = (io) => {
   io.on("connection", (socket) => {
-    //console.log("⚡ User connected:", socket.id);
+    console.log("⚡ User connected:", socket.id);
 
-    // Send data every 60 seconds
-    const interval = setInterval(async () => {
+    // Function to fetch and send data
+    const sendData = async () => {
       try {
         const products = await productModel.find({});
-        //console.log('data send', products);
         socket.emit("productData", products);
       } catch (err) {
         socket.emit("error", { message: "Error fetching data" });
       }
-    }, 60000); // 60000ms = 60 seconds
+    };
+
+    // Send data immediately
+    sendData();
+
+    // Then send data every 60 seconds
+    const interval = setInterval(sendData, 60000);
 
     socket.on("disconnect", () => {
       console.log("❌ User disconnected:", socket.id);
-      clearInterval(interval); // stop interval when client disconnects
+      clearInterval(interval);
     });
   });
 };
